7709ee2ecb0fce4a0dad3b1b872fed5d8d0b74a4
Jolien De Letter <joliendeletter@gmail.com>
2020-06-12 11:10:17 +0200

    Structure statisticscontroller

--- BankApp/Controllers/StatisticsController.cs
+++ BankApp/Controllers/StatisticsController.cs
@@ -13,22 +13,23 @@ namespace BankApp.Controllers
     public class StatisticsController : Controller
     {
         private readonly IExpenseDatabase _expenseDatabase;
+        private readonly IEnumerable<Expense> _expenses;
 
         public StatisticsController(IExpenseDatabase expenseDatabase)
         {
             _expenseDatabase = expenseDatabase;
+            _expenses = _expenseDatabase.GetExpenses();
         }
         [HttpGet]
         public IActionResult Index()
         {
             StatisticsIndexViewModel vm = new StatisticsIndexViewModel()
             {
-                Expenses = _expenseDatabase.GetExpenses(),
-                HighestExpense = _expenseDatabase.GetExpenses().OrderByDescending(x => x.Amount).First(),
-                LowestExpense = _expenseDatabase.GetExpenses().OrderBy(x => x.Amount).First(),
-                // x => x.Date.Date.Year
-                MonthlyExpenses = _expenseDatabase.GetExpenses().GroupBy(x => x.Date.Date.Month).Select(g => new GroupedExpenses { Date = new DateTime(2020, g.Key,01), Amount = g.Sum(m => m.Amount) }).OrderBy(x =>x.Date),
-                HighestDayExpense = _expenseDatabase.GetExpenses().GroupBy(x => x.Date.Date).Select(x => new GroupedExpenses{ Date = x.Key, Amount = x.Sum(m => m.Amount)}).OrderByDescending(x => x.Amount).First()
+                Expenses = _expenses,
+                HighestExpense = _expenses.OrderByDescending(x => x.Amount).First(),
+                LowestExpense = _expenses.OrderBy(x => x.Amount).First(),
+                MonthlyExpenses = _expenses.GroupBy(x => x.Date.Date.Month).Select(g => new GroupedExpenses { Date = new DateTime(2020, g.Key,01), Amount = g.Sum(m => m.Amount) }).OrderBy(x =>x.Date),
+                HighestDayExpense = _expenses.GroupBy(x => x.Date.Date).Select(x => new GroupedExpenses{ Date = x.Key, Amount = x.Sum(m => m.Amount)}).OrderByDescending(x => x.Amount).First()
             };
             return View(vm);
         }
